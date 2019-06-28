acab917d74f3197cda7124725c1849a5d278c3a8
lmencos <lmencos@hotmail.com>
2019-06-28 12:24:27 -0500

    #d project start

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const navBar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Gallery</li>
+          <li>Projects</li>
+          <li>Contact us</li>
+          <li>Sign in</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+export default navBar;
