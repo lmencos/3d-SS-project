0a15aef774fceeafc4ae0f34a2dac9edbf099eac
lmencos <lmencos@hotmail.com>
2019-07-13 00:39:35 -0500

    Updates

---
+++ precache-manifest.d8b95a2d7bbc1d2cd942cfed9d99873f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e589b5046479bc856a952b51d50418fd",
+    "url": "/3d-SS-project/index.html"
+  },
+  {
+    "revision": "bd8819df78057e17714b",
+    "url": "/3d-SS-project/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4d45e7aedb04d13231f3",
+    "url": "/3d-SS-project/static/css/main.94e22498.chunk.css"
+  },
+  {
+    "revision": "bd8819df78057e17714b",
+    "url": "/3d-SS-project/static/js/2.6aa2966c.chunk.js"
+  },
+  {
+    "revision": "4d45e7aedb04d13231f3",
+    "url": "/3d-SS-project/static/js/main.08959a21.chunk.js"
+  },
+  {
+    "revision": "0ddb389d99c50458e236",
+    "url": "/3d-SS-project/static/js/runtime~main.779a3f19.js"
+  },
+  {
+    "revision": "238766bf1c5d13d5e5f8404859742085",
+    "url": "/3d-SS-project/static/media/arid2_bk.238766bf.jpg"
+  },
+  {
+    "revision": "5d5b8be3f8623ac980aa6d14e5ddadce",
+    "url": "/3d-SS-project/static/media/arid2_dn.5d5b8be3.jpg"
+  },
+  {
+    "revision": "6a76a7ae96e67f7cbd859ac5ecfda8a8",
+    "url": "/3d-SS-project/static/media/arid2_ft.6a76a7ae.jpg"
+  },
+  {
+    "revision": "2a53a84897cd4d6ef43768711d1606a9",
+    "url": "/3d-SS-project/static/media/arid2_lf.2a53a848.jpg"
+  },
+  {
+    "revision": "64c0edaadcb25377b5e728691b98a4b2",
+    "url": "/3d-SS-project/static/media/arid2_rt.64c0edaa.jpg"
+  },
+  {
+    "revision": "e0e3a4fd5eeb154657063c1b948b9190",
+    "url": "/3d-SS-project/static/media/arid2_up.e0e3a4fd.jpg"
+  },
+  {
+    "revision": "b79f0bec871a6ec65a2697e9be4fb38a",
+    "url": "/3d-SS-project/static/media/scene.b79f0bec.gltf"
+  }
+]);
