970107c97e8e1416d5f08e37fb2fd40b6e687e83
lmencos <lmencos@hotmail.com>
2019-08-14 13:42:20 -0500

    Auth init

---
+++ src/components/Auth.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+  render () {
+    return (
+      <div>
+        <form>
+          
+        </form>
+      </div>
+    );
+  }
+}
+export default Auth;
