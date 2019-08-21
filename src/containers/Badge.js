4c295a0fc33c9478683060c3c6b004e219f8c155
lmencos <lmencos@hotmail.com>
2019-08-20 19:20:10 -0500

    List Gallery missing src

--- src/containers/Badge.js
+++ src/containers/Badge.js
@@ -4,41 +4,42 @@ import './styles/Badge.css'
 
 class Badge extends Component {
   render(){
-    return(
+    
+    return(        
       <div className="Badge">
-        <div className="">
-
-        </div>
 
         <div className="Badge__header">
           <h3>Chichén Itzá</h3>
         </div> 
        
-        <div className=" Badge__section-name" >
-          <img
-            className="Badge__avatar" 
-            src={Chichen} 
-            width="40%" 
-            alt="Real Model"/>
-
-          <p> <strong className="Badge__type"> {this.props.badgeType} </strong><br/>
-          <small>Artist: {this.props.artist} </small><br/>
-          <h6>Twitter: @{this.props.twitter}</h6>
-          </p>
+        <div className="Badge__section-name">
+        
+          <div className=" Badge__section-name"  >
+            <img
+              className="Badge__avatar" 
+              src={Chichen} 
+              width="40%" 
+              alt="Real Model"/>
+          
+            <span> <strong className="Badge__type"> {this.props.badgeType} </strong><br/>
+            <small>Artist: {this.props.artist} </small><br/>
+            <h6>Twitter: @{this.props.twitter}</h6>
+            </span>
+          </div>
+          
+          
+          <div className=" Badge__section-name" >
+            <img
+              className="Badge__avatar" 
+              src={Chichen} 
+              width="40%" 
+              alt="Real Model"/>
         </div>
-  
-  
-        <div className=" Badge__section-name" >
-          <img
-            className="Badge__avatar" 
-            src={Chichen} 
-            width="40%" 
-            alt="Real Model"/>
 
-          <p> <strong className="Badge__type"> {this.props.badgeType} </strong><br/>
+          <span> <strong className="Badge__type"> {this.props.badgeType} </strong><br/>
           <small>Artist: {this.props.artist}</small><br/>
           <h6>Twitter: @{this.props.twitter}</h6>
-          </p>
+          </span>
         </div>
 
         <div className="Badge__footer">
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import Badge from '../containers/Badge';
+import BadgesList from '../containers/BadgesList';
+
 
 class Gallery extends React.Component {
+  state={
+    data:[
+      {
+        id: '1',
+        badgeTitle: "About space",
+        badgeTypeA: 'Real model, photo',
+        artistA: 'FDCabrera',
+        twitterA: 'fdCabrera',
+        badgeTypeB: '3D object, render',
+        artistB: 'FDCabrera',
+        twitterB: 'fdCabrera',
+        modelImageA: "../containers/images/siluetajpg.jpg",
+      },
+      {
+        id: '2',
+        badgeTitle: "lookat",
+        badgeTypeA: 'Real model, photo',
+        artistA: 'FDCabrera',
+        twitterA: 'fdCabrera',
+        badgeTypeB: '3D object, render',
+        artistB: 'FDCabrera',
+        twitterB: 'fdCabrera',
+        modelImageB: "../containers/images/siluetajpg.jpg",
+      }
+    ]
+  };
+
   render (){
     return (
       <div>
+        <BadgesList badges={this.state.data} />
+
         <Badge 
           badgeType="Real Model photo" 
           artist="Daniel Cabrera G."
