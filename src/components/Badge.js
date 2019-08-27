d3086a13697578ad8fd18818457705a3b459068c
lmencos <lmencos@hotmail.com>
2019-08-27 17:37:15 -0500

    gallery styling ok

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,46 +1,58 @@
 import React from 'react';
 import Chichen from '../back-images/chichen.jpg';
 import './styles/Badge.css';
+import mascara from '../back-images/mask-05.png';
+import mascara1 from '../back-images/mask-06.png';
+import enviroment from '../back-images/enviroment.png';
+
 
 const Badge =(props) => {
     return(        
       <div className="Badge">
 
-        <div className="Badge__header">
-        {console.log("badgeProps",props.badgeTitle)}
-          <h3>{props.badgeTitle}</h3>
-        </div> 
-       
-        <div className="Badge__section-name">
-        
-          <div className=" Badge__section-name"  >
-            <img
-              className="Badge__avatar" 
-              src={props.imageUrlA} 
-              width="40%" 
-              alt="Real Model"/>
+          <div className="Badge__header">
+          {console.log("badgeProps",props.badgeTitle)}
+            <h3>{props.badgeTitle}</h3>
+          </div> 
+        <div className=" Badge__section-content1">
           
-            <span> <strong className="Badge__type"> {props.badgeTypeA} </strong><br/>
-            <small>Artist: {props.artistA} </small><br/>
-            <h6>Twitter: @{props.twitterA}</h6>
-            </span>
+            <div className="Badge-image">
+              <label className="Badge__type"> {props.badgeTypeA}</label> 
+              <div>
+                <img
+                  className="Badge__avatar" 
+                  src={mascara} 
+                  width="50%" 
+                  alt="Real Model"/>
+              </div>
+            
+              <div className="Artist"> 
+                Artist: {props.artistA}<br/>
+                <h6>Twitter: @{props.twitterA}</h6>
+              </div>
+            </div>
+        </div>
+          
+        <div className=" Badge__section-content1">
+          
+          <div className="Badge-image">
+            <label className="Badge__type"> {props.badgeTypeB}</label> 
+            <div>
+              <img
+                className="Badge__avatar" 
+                src={mascara1} 
+                width="50%" 
+                alt="Real Model"/>
+            </div>
+          
+            <div className="Artist"> 
+              Artist: {props.artistB}<br/>
+              <h6>Twitter: @{props.twitterB}</h6>
+            </div>
           </div>
-          
-          
-          <div className=" Badge__section-name" >
-            <img
-              className="Badge__avatar" 
-              src={Chichen} 
-              width="40%" 
-              alt="Real Model"/>
-        </div>
+      </div>
 
-          <span> <strong className="Badge__type"> {props.badgeTypeB} </strong><br/>
-          <small>Artist: {props.artistB}</small><br/>
-          <h6>Twitter: @{props.twitterB}</h6>
-          </span>
-        </div>
-
+       
         <div className="Badge__footer">
           <h6>Real Model vs 3D render</h6>
         </div>
